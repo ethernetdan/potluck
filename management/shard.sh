09c5918f733b668d1596253dba72997b7cf631fd
Dan Gillespie <danielg@umd.edu>
2014-08-03 09:40:28 -0700

    removed unnecessary file

--- management/shard.sh
+++
@@ -1,1 +0,0 @@
-docker run -d -p 4249:8080 -p 8038:28015 -p 2098:29015 -v ~/data:/data dockerfile/rethinkdb rethinkdb --bind all --canonical-address `curl -s ipecho.net/plain` --machine-name `hostname | sed 's/-/_/g'`
