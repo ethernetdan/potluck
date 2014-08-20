6d602b9b827e5ca7ffde0d1ee6bfc1e9a5eeccd6
Dan Gillespie <danielg@umd.edu>
2014-08-20 11:06:36 -0700

    reformatted appSpawner

--- management/appSpawner.rb
+++ management/appSpawner.rb
@@ -6,38 +6,39 @@ include RethinkDB::Shortcuts
 conn = r.connect(:host => ENV['CENTRAL'], :port => 28015, :db => 'management').repl
 
 r.table('nodes').changes().filter{|row| # catches only changes that add/delete nodes
-		(row['old_val'].eq(nil))# || row['new_val'].eq(nil))
-	}.run(conn).each{|change|
-	p(change)
+  (row['old_val'].eq(nil))# || row['new_val'].eq(nil))
+}.run(conn).each{|change|
+  p(change)
 
-        repo = change["new_val"]["repo"]
-	ipToAdd = change["new_val"]["ip"]
-	id = change["new_val"]["id"]
-	# every change here should lead us to rewrite the nginX config
-	# /etc/nginx/sites-enabled/app.conf
-	config = ""
+  repo = change["new_val"]["repo"]
+  ipToAdd = change["new_val"]["ip"]
+  id = change["new_val"]["id"]
 
-        conf = File.open('/etc/nginx/servers.conf').read
-	conf.each_line do |li|
-		config += li
-  		if (li['upstream cluster {'])
-  			puts "found beginning of config section: add an ip address here"
-  			config += "server #{ipToAdd}:1995;\n"
-  		end
-	end
+  # every change here should lead us to rewrite the nginX config
+  # /etc/nginx/sites-enabled/app.conf
+  config = ""
 
-        exec("git clone #{repo} /home/app/#{id}") if fork.nil?
-        exec("git chown 9999:9999 -R /home/app/#{id}") if fork.nil?
+  conf = File.open('/etc/nginx/servers.conf').read
+  conf.each_line do |li|
+    config += li
+    if (li['upstream cluster {'])
+      puts "found beginning of config section: add an ip address here"
+      config += "server #{ipToAdd}:1995;\n"
+    end
+  end
 
-	# edit config
-        File.write('/etc/nginx/servers.conf', config)
-	# reload nginx
-        exec("/etc/init.d/nginx reload") if fork.nil?
+  # clone and configure application
+  exec("git clone #{repo} /home/app/#{id}") if fork.nil?
+  exec("git chown 9999:9999 -R /home/app/#{id}") if fork.nil?
 
+  # add new host to potluck cluster
+  File.write('/etc/nginx/servers.conf', config)
+  # reload nginx
+  exec("/etc/init.d/nginx reload") if fork.nil?
 }
-
-
-#
+#############################
+# CODE FOR SERVER REMOVAL   #
+#############################
 #if (change["new_val"] == nil)
 #		config = ""
 #
